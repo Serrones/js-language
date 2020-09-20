983286cf8755c7f34af471c1af83cedfb85a502f
Serrones <fabioserrones@gmail.com>
2020-09-20 19:55:24 -0300

    variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+// This is a comment line
+
+
+console.log("Working with variables");
+
+
+// Javascript is case sensitive
+
+age = 43;
+
+Age = 40;
+
+console.log("age:", age);
+
+console.log("Age:", Age);
