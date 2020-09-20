1ad73d12254ce57bcc73fea5a7445165afa45f3c
Serrones <fabioserrones@gmail.com>
2020-09-20 20:06:40 -0300

    operations

---
+++ operations.js
@@ -0,0 +1,30 @@
+// arithmetic operations
+
+console.log("arithmetic operations");
+
+console.log(2+2);
+
+console.log(2 + 2 * 7);
+
+console.log((2+2)* 7);
+
+// Type conversion
+
+console.log("Type Conversion");
+
+console.log("2")
+
+// Implicit conversion
+
+console.log("year " + 2020);
+
+console.log("2" + "2");
+
+console.log("10" / "2");
+
+// Explicit conversion
+console.log(parseInt("2") + parseInt("5"));
+console.log(parseFloat("2.5") + parseFloat("5.75"));
+
+// Returns Na7N - not a number
+console.log("Sky" / "2");
