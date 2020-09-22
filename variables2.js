47cca8efd408f4fa06643d874eba4a250c34a7f9
Serrones <fabioserrones@gmail.com>
2020-09-22 18:52:32 -0300

    Variables 2

---
+++ variables2.js
@@ -0,0 +1,17 @@
+console.log("Working with variables");
+
+const firstName = "Fábio";
+
+const surname = "Serrones";
+
+const fullName = `${firstName} ${surname}`;
+
+console.log(firstName + surname);
+
+console.log(firstName, surname);
+
+console.log(firstName + " " + surname);
+ 
+console.log(`My name is ${firstName} ${surname}`);
+
+console.log(`My name is ${fullName}`);
